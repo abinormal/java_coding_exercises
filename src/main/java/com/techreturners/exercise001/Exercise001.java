f07e5a1e598f4cb66802a49ceee6db792622f689
Abi Sidford <asidford@gmail.com>
2020-09-05 12:46:21 +0100

    Fixed bug in addVat - truncate trailing decimal places.

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,6 +1,7 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.text.DecimalFormat;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
@@ -16,7 +17,13 @@ public class Exercise001 {
         // Calculate 50% of 120: int k = (int)(120*(50.0f/100.0f));
         // more generally: int k = (int)(value*(percentage/100.0f));
         double vat = (originalPrice*(vatRate/100.0f) );
-        return originalPrice + vat;
+
+       // double total = originalPrice + vat;
+
+        // Format to truncate trailing decimal places
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        return Double.parseDouble(df.format(originalPrice + vat));
     }
 
     public String reverse(String sentence) {
